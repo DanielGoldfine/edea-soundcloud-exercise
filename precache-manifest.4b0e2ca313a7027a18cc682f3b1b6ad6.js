8cdbab20b731cc9fcd403a366d655a53d79c57cd
Your Name <daniel720p@gmail.com>
2020-08-17 09:31:17 +0300

    Updates

---
+++ precache-manifest.4b0e2ca313a7027a18cc682f3b1b6ad6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dd3c04783e49cc35fbc0e15fa6518a",
+    "url": "/edea-soundcloud-exercise/index.html"
+  },
+  {
+    "revision": "092268106afd5a9e7a72",
+    "url": "/edea-soundcloud-exercise/static/css/main.9d919576.chunk.css"
+  },
+  {
+    "revision": "dd87b2476b1153794f18",
+    "url": "/edea-soundcloud-exercise/static/js/2.df24eab6.chunk.js"
+  },
+  {
+    "revision": "6b876e448e44c278309fce9ee4d4d733",
+    "url": "/edea-soundcloud-exercise/static/js/2.df24eab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092268106afd5a9e7a72",
+    "url": "/edea-soundcloud-exercise/static/js/main.a20f7ea8.chunk.js"
+  },
+  {
+    "revision": "e7d395fea73fa67274d5",
+    "url": "/edea-soundcloud-exercise/static/js/runtime-main.e028e4f0.js"
+  },
+  {
+    "revision": "cbd4e701269338259ee0b39a0b768167",
+    "url": "/edea-soundcloud-exercise/static/media/RobotoCondensed-Bold.cbd4e701.ttf"
+  },
+  {
+    "revision": "885e5558c90864ddb72ad486f36470cc",
+    "url": "/edea-soundcloud-exercise/static/media/RobotoCondensed-Light.885e5558.ttf"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/edea-soundcloud-exercise/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "303ae944f56723075b36d9dc8ac9ad01",
+    "url": "/edea-soundcloud-exercise/static/media/play-circle-solid.303ae944.svg"
+  }
+]);
